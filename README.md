25be2a82abdcd018824e128cb37b9a17c15be1fa
NormalPlayerJSH <jsh010904@gmail.com>
2021-07-05 15:48:34 +0900

    보일러플레이트 적용

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LOLGO_Frontend
 
-Frontend
+Need Node 14 or 15
+
+현재 보일러플레이트 정상 적용 및 IPC 통신 확인 완료
---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
+import icon from '../assets/icon.svg';
+import './App.global.css';
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a href="asdf">
+          <button
+            type="button"
+            onClick={() => ipcRenderer.send('asdf', { msg: 'asdf' })}
+          >
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Send asdf
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}

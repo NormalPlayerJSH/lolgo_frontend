eeb849a3d02e14cb2a766d6194f2192d97e8f037
NormalPlayerJSH <jsh010904@gmail.com>
2021-07-05 16:03:38 +0900

    airbnb 룰 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,37 @@ import { ipcRenderer } from 'electron';
 import icon from '../assets/icon.svg';
 import './App.global.css';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <button
-          type="button"
-          onClick={() => ipcRenderer.send('asdf', { msg: 'asdf' })}
-        >
-          <span role="img" aria-label="books">
-            📚
-          </span>
-          Send asdf
-        </button>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+const Hello = () => (
+  <div>
+    <div className="Hello">
+      <img width="200px" alt="icon" src={icon} />
     </div>
-  );
-};
+    <h1>electron-react-boilerplate</h1>
+    <div className="Hello">
+      <button
+        type="button"
+        onClick={() => ipcRenderer.send('asdf', { msg: 'asdf' })}
+      >
+        <span role="img" aria-label="books">
+          📚
+        </span>
+        Send asdf
+      </button>
+      <a
+        href="https://github.com/sponsors/electron-react-boilerplate"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button type="button">
+          <span role="img" aria-label="books">
+            🙏
+          </span>
+          Donate
+        </button>
+      </a>
+    </div>
+  </div>
+);
 
 export default function App() {
   return (

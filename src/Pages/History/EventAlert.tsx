9c2634f3d2df9571c27333583600c6bb813553e5
NormalPlayerJSH <jsh010904@gmail.com>
2021-10-09 05:11:20 +0900

    변경된 아이콘 적용

--- src/Pages/History/EventAlert.tsx
+++ src/Pages/History/EventAlert.tsx
@@ -6,20 +6,13 @@ import {
 import { getChampionImage } from '../../Meta/DataDragon';
 import BlueBlank from '../../Meta/blueBlank.png';
 import RedBlank from '../../Meta/redBlank.png';
-import Empty from '../../Meta/empty.png';
+import getIcon from '../../Meta/icons';
 
-const towerIcon = BlueBlank;
-const dragonIcon = RedBlank;
-const baronIcon = Empty;
-const inhibitorIcon = getChampionImage(1);
-const heraldIcon = getChampionImage(2);
+const imgToIcon = (src:string) => <img src={src} className={styles.iconImg} alt="" />;
 
-const KillIcon = (team:TeamType) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="40rem" height="40rem" viewBox="0 0 24 24" fill={`${team === 100 ? '#425cea' : '#ba5657'}`}>
-    <path d="M0 0h24v24H0V0z" fill="none" />
-    <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z" />
-  </svg>
-);
+const {
+  baronIcon, heraldIcon, towerIcon, dragonIcon, inhibitorIcon, killIcon,
+} = getIcon('40rem');
 
 function EventAlert(props: {
   data:EachKillData|EachEliteData|EachTowerData,
@@ -27,32 +20,33 @@ function EventAlert(props: {
 }) {
   const { data, info } = props;
   let team:TeamType = 100;
-  let left = '';
-  let right = '';
+  let left = <></>;
+  let right = <></>;
   if ('victimId' in data) {
     const leftChampion = info[data.killerId].championId;
     const rightChampion = info[data.victimId].championId;
-    left = getChampionImage(leftChampion);
-    right = getChampionImage(rightChampion);
+    left = imgToIcon(getChampionImage(leftChampion));
+    right = imgToIcon(getChampionImage(rightChampion));
     team = info[data.killerId].teamId;
   } else if ('towerType' in data) {
     const { teamId, towerType } = data;
     team = teamId === 100 ? 200 : 100;
-    left = teamId === 100 ? RedBlank : BlueBlank;
-    right = towerType === 'INHIBITOR_BUILDING' ? inhibitorIcon : towerIcon;
+    left = imgToIcon(teamId === 100 ? RedBlank : BlueBlank);
+    right = towerType === 'INHIBITOR_BUILDING' ? inhibitorIcon(teamId) : towerIcon(teamId);
   } else {
     const { monsterType, killerId } = data;
     const leftChampion = info[killerId].championId;
-    left = getChampionImage(leftChampion);
-    if (monsterType === 'BARON_NASHOR') right = baronIcon;
-    else if (monsterType === 'RIFTHERALD') right = heraldIcon;
-    else right = dragonIcon;
+    left = imgToIcon(getChampionImage(leftChampion));
+    team = info[killerId].teamId;
+    if (monsterType === 'BARON_NASHOR') right = baronIcon();
+    else if (monsterType === 'RIFTHERALD') right = heraldIcon();
+    else right = dragonIcon(monsterType);
   }
   return (
     <div className={styles.eventAlertDiv}>
-      <img src={left} className={styles.iconImg} alt="" />
-      {KillIcon(team)}
-      <img src={right} className={styles.iconImg} alt="" />
+      {left}
+      {killIcon(team)}
+      {right}
     </div>
   );
 }
